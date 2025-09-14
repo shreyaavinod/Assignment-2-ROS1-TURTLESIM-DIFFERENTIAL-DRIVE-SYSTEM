9cbc7e2168222f5a60f124c1352286fe25be554f
Shreyaa <shreyaavinod96@gmail.com>
2025-09-14 13:06:40 +0530

    Initial commit: Turtlesim differential drive forward kinematics

---
+++ src/wheel_velocity_pub.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <turtle_diff_drive/wheel_velocity.h>
+
+int main(int argc, char **argv)
+{
+	ros::init(argc,argv,"wheel_velocity_pub");
+	ros::NodeHandle node1;
+	
+	ros::Publisher pub=node1.advertise<turtle_diff_drive::wheel_velocity>("wheel_vel",10);
+	
+	ros::Rate loop_rate(1);
+	
+	while(ros::ok())
+	{
+		turtle_diff_drive::wheel_velocity msg;
+		msg.omega_l=10;
+		msg.omega_r=10;
+		
+		pub.publish(msg);
+		ROS_INFO("Publishing: omega_left=%.2f, omega_right=%.2f", msg.omega_l, 			msg.omega_r); 
+		loop_rate.sleep();
+	}
+	return 0;
+	
+	
+}
